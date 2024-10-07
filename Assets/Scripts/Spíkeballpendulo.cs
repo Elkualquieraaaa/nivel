5a5cf767891d9436c1d5b60c78127b2393f010cb
Elkualquieraaaa <triplehak88@gmail.com>
2024-10-07 11:42:27 -0500

    sumatoria

--- Assets/Scripts/Spíkeballpendulo.cs
+++ Assets/Scripts/Spíkeballpendulo.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Spíkeballpendulo : MonoBehaviour
 {
+    public List<Transform> objetivos = new List<Transform>();
+    [SerializeField] float velocity;
+    int objetivoactual = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Transform t in objetivos)
+        {
+            t.SetParent(null);
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, objetivos[objetivoactual].rotation, velocity * Time.deltaTime);
+
+
+        if (transform.rotation == objetivos[objetivoactual].rotation)
+        {
+            objetivoactual++;
+            if (objetivoactual > objetivos.Count - 1)
+            {
+                objetivoactual = 0;
+            }
+        }
     }
 }
